6ccdf4cd3363821b2eae455afcea1acd6d70ceaf
Andrew Craig <andrewcraig2@gmail.com>
2021-01-10 15:56:01 -0600

    fixed bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,10 @@ function generatePassword() {
   var chooseLower = confirm("Do you want lower case")
   var chooseUpper = confirm("Do you want upper case")
   var chooseNumber = confirm ("Do you want numbers")
-  var chooseSymbol = confirm ("Do you like symbols")
+  var chooseSymbol = confirm ("Do you want symbols")
   var upperArray  = "ABCDEFGHIJKLMNOPQRSTUYWXYZ".split("")
   var lowerArray = "abcdefghijklmnopqrstuvwxyz".split("")
-  var numbersArray = "0123456789".split("")
+  var numberArray = "0123456789".split("")
   var symbolArray = "!&*$#{}=+@()?".split("")
   var choosePassLength = prompt("How long do you want password?")
   choosePassLength = parseInt(choosePassLength)
@@ -14,39 +14,37 @@ function generatePassword() {
   
   if(chooseUpper) {
     for (let index = 0; index < upperArray.length; index++) {
-      const element = UpperArray[index];
+      const element = upperArray[index];
       userChosenArray.push(element);
   }
-
 }
  if(chooseLower) {
    for (let index = 0; index < lowerArray.length; index++)  {
-     const element = LowerArray[index];
+     const element = lowerArray[index];
      userChosenArray.push(element);
    }
  }
 
  if(chooseNumber) {
-  for (let index = 0; index < NumberArray.length; index++)  {
+  for (let index = 0; index < numberArray.length; index++)  {
     const element = numberArray[index];
     userChosenArray.push(element);
   }
 }
   if (chooseSymbol) {
-  for (let index = 0; index < symbol.length; index++) {
+  for (let index = 0; index < symbolArray.length; index++) {
     const element = symbolArray[index];
     userChosenArray.push(element)
   }
   console.log(userChosenArray)
 }
 var createPassword = ""
-for (let index = 0; index < choosePassLength; index++) {
-  var randomIndex = Math.floor(Math.random() * userChosenArray.length)
-  createPassword += userChosenArray[randomIndex]
+  for (let index = 0; index < choosePassLength; index++) {
+    var randomIndex = Math.floor(Math.random() * userChosenArray.length)
+    createPassword += userChosenArray[randomIndex]
+  }
+  return createPassword
 }
-return createPassword
-}
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
